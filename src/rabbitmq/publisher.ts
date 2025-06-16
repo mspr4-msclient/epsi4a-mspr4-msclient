4fa235259fb53ae96fe05fecbb8282e9e3053736
AntoineLetailleur <ant.letailleur@gmail.com>
2025-06-16 18:19:44 +0200

    Update dockerignore + change value for durable into publisher

--- src/rabbitmq/publisher.ts
+++ src/rabbitmq/publisher.ts
@@ -10,7 +10,7 @@ export async function publishMessage(msg: unknown, exchangeName: string, exchang
       if (error0) { throw error0; }
       connection.createChannel(function(error1, channel) {
         if (error1) { throw error1; }
-        channel.assertExchange(exchangeName, exchangeType, {durable: false})
+        channel.assertExchange(exchangeName, exchangeType, {durable: true})
         channel.publish(exchangeName, exchangeType, Buffer.from(JSON.stringify(msg)));
         console.log(" [x] Sent %s", JSON.stringify(msg));
       });
