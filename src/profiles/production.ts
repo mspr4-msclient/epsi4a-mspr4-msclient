dbcf5e09fa8e6b6137b61d600b26597ad6851200
AntoineLetailleur <ant.letailleur@gmail.com>
2025-07-09 13:34:49 +0200

    Refacto workflows

--- src/profiles/production.ts
+++ src/profiles/production.ts
@@ -1,11 +1,11 @@
 module.exports = {
     // NODE_ENV: process.env.NODE_ENV,
     PORT: 8080,
-    CLIENT_URL: "https://mspr4-cloud-run-service-321153931988.us-central1.run.app",
+    CLIENT_URL: "https://epsi4a-mspr4-msclient-321153931988.europe-west1.run.app",
     RABBITMQ_URL:"amqp://34.42.114.30",
     LOGSTASH_URL:"http://34.42.114.30:5044",
     // ELASTICSEARCH_URL:process.env.ELASTICSEARCH_URL,
     // CLIENT_MONGO_CONNECTION_STRING: process.env.CLIENT_MONGO_CONNECTION_STRING,
-    PROMETHEUS_EXPORTER_URL:"https://mspr4-cloud-run-service-321153931988.us-central1.run.app/metrics",
+    PROMETHEUS_EXPORTER_URL:"https://epsi4a-mspr4-msclient-321153931988.europe-west1.run.app/metrics",
     OLTP_TRACE_EXPORTER_URL: 'http://34.42.114.30:4318/v1/traces'
 };
